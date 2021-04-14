b6bf2447bb9177cf1dc4fbe79708a16c782c6448
Bhawna Gupta <onlybhawnagupta@gmail.com>
2021-04-14 20:16:56 +0530

    Delete usersRoute.js

--- routes/usersRoute.js
+++
@@ -1,9 +0,0 @@
-const express =require('express');
-const myFunctions=require('../controllers/usersController')
-const router = express.Router();
-
-// users Routes
-router.post('/signup',myFunctions.createUser);
-router.post('/login', myFunctions.siginUser);
-
-module.exports= router;
